c45ea5d20e1379784d07b5c3eb3f1d4109cc60b1
nikitamelni <nekit.mc@gmail.com>
2018-04-26 18:21:48 -0400

    removing the comments

--- src/Sitecore.Support.219063/Cloud/Xdb/CloudBasedTransportFactory.cs
+++ src/Sitecore.Support.219063/Cloud/Xdb/CloudBasedTransportFactory.cs
@@ -10,11 +10,6 @@ namespace Sitecore.Support.Cloud.Xdb
 {
   public class CloudBasedTransportFactory : ApiKeyHttpTransportFactory
   {
-    //public CloudBasedTransportFactory(string serviceUrl, string timeout, string certificateThumbprint, bool requireHttps, string connectionStringName, string headerName, string minimalApiKeyLength) : base(serviceUrl, timeout, requireHttps, connectionStringName, headerName, minimalApiKeyLength)
-    //{
-    //  Log.Info("Sitecore.Support.219063 serviceUrl: " + serviceUrl, this);
-    //  SslCertificateValidationListener.Initialize(certificateThumbprint);
-    //}
     public CloudBasedTransportFactory(string timeout, string serviceUrl, string certificateThumbprint, bool requireHttps, string connectionStringName, string headerName, string minimalApiKeyLength) : base(serviceUrl, timeout, requireHttps, connectionStringName, headerName, minimalApiKeyLength)
     {
       SslCertificateValidationListener.Initialize(certificateThumbprint);
