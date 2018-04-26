514c6ae5858c2c9cb96003a7958e21088af23142
DK\nim <nim@sitecore.net>
2018-04-26 18:19:09 -0400

    Changed to the supported constructor of the new ApiKeyHttpTransportFactory

---
+++ src/Sitecore.Support.219063/Cloud/Xdb/CloudBasedTransportFactory.cs
@@ -0,0 +1,23 @@
+﻿using Sitecore.Analytics.Commons;
+using Sitecore.Cloud.Xdb;
+using Sitecore.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Support.Cloud.Xdb
+{
+  public class CloudBasedTransportFactory : ApiKeyHttpTransportFactory
+  {
+    //public CloudBasedTransportFactory(string serviceUrl, string timeout, string certificateThumbprint, bool requireHttps, string connectionStringName, string headerName, string minimalApiKeyLength) : base(serviceUrl, timeout, requireHttps, connectionStringName, headerName, minimalApiKeyLength)
+    //{
+    //  Log.Info("Sitecore.Support.219063 serviceUrl: " + serviceUrl, this);
+    //  SslCertificateValidationListener.Initialize(certificateThumbprint);
+    //}
+    public CloudBasedTransportFactory(string timeout, string serviceUrl, string certificateThumbprint, bool requireHttps, string connectionStringName, string headerName, string minimalApiKeyLength) : base(serviceUrl, timeout, requireHttps, connectionStringName, headerName, minimalApiKeyLength)
+    {
+      SslCertificateValidationListener.Initialize(certificateThumbprint);
+    }
+  }
+}
